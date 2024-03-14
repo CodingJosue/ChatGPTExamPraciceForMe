604e3c64890fb1e4cd7e10b00d00b6beb98a5cf8
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-14 16:02:39 -0400

    Stars pattern I did not understand this one at all I will probably try later to do it on my onw since I watch a tutorial

--- src/main/java/Question4/ArraysPatternExamples.java
+++ src/main/java/Question4/ArraysPatternExamples.java
@@ -1,18 +1,23 @@
 package Question4;
 
+import java.util.Arrays;
+
 public class ArraysPatternExamples {
     public static void main(String[] args) {
         // Method that find the sum diagonal from top right to bottom left
         int[][] arrays = {
-                {1,45,7},
-                {1,45,2},
-                {1,3,45}
+                {0, 45, 0},
+                {1, 45, 2},
+                {0, 3, 0}
         };
         topRightBottomleftSum(arrays);
 
-        topLeftBottomRight(arrays);
+        // topLeftBottomRight(arrays);
 
-        reverseTopLeftBottomRight(arrays);
+        //reverseTopLeftBottomRight(arrays);
+
+        sumInXPattern(arrays);
+        printStarPattern();
     }
 
     public static void topRightBottomleftSum(int[][] arrays) {
@@ -25,31 +30,61 @@ public class ArraysPatternExamples {
         System.out.println(diagonalSum);
     }
 
-    public static void BottomLeftToTopRight(int[][] arrays){
+    public static void BottomLeftToTopRight(int[][] arrays) {
         int inverseDiagonalSum = 0;
-        for(int i = 0; i < arrays.length; i++){
-            for(int j = arrays.length - 1; j >= 0; j--){
-                if(i == j) inverseDiagonalSum += arrays[i][j];
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = arrays.length - 1; j >= 0; j--) {
+                if (i == j) inverseDiagonalSum += arrays[i][j];
             }
         }
         System.out.println(inverseDiagonalSum);
     }
-    public static void topLeftBottomRight(int[][] arrays){
+
+    public static void topLeftBottomRight(int[][] arrays) {
         int sumOfThat = 0;
-        for(int i = 0;  i < arrays.length; i++ ){
-            for (int j = arrays.length - 1; j >= 0; j--){
-                if((i + j )== arrays.length - 1) sumOfThat+= arrays[i][j];
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = arrays.length - 1; j >= 0; j--) {
+                if ((i + j) == arrays.length - 1) sumOfThat += arrays[i][j];
             }
         }
-        System.out.println(sumOfThat );
+        System.out.println(sumOfThat);
     }
-    public static void reverseTopLeftBottomRight(int[][] arrays){
+
+    public static void reverseTopLeftBottomRight(int[][] arrays) {
         int suumOfThat = 0;
-        for(int i = arrays.length - 1; i >= 0; i-- ){
-            for(int j = 0; j < arrays.length; j++){
-                if(Math.abs(i - j) == arrays.length - 1 || (i + j) == arrays.length - 1) suumOfThat += arrays[i][j];
+        for (int i = arrays.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arrays.length; j++) {
+                if (Math.abs(i - j) == arrays.length - 1 || (i + j) == arrays.length - 1) suumOfThat += arrays[i][j];
             }
         }
         System.out.println(suumOfThat);
+    }
+
+    public static void sumInXPattern(int[][] arrays) {
+        int sumOfThat = 0;
+        int i, v;
+        for (i = 0, v = arrays.length - 1; i < arrays.length; i++, v--) {
+            for (int j = 0; j < arrays.length; j++) {
+                if (i == j || v == j) sumOfThat += arrays[i][j];
+            }
+        }
+        System.out.println(sumOfThat);
+    }
+
+    public static void findRepeatedElements(int[] arrays) {
+        Arrays.sort(arrays);
+
+    }
+
+    public static void printStarPattern() {
+        int num = 0;
+
+        for (int i = 0; i < 11; i++) {
+            for(int j = 11- i - 1; j >= 1; j--){
+                System.out.print(" ");
+            }
+            for (int t = 1; t < i; t++) System.out.print("* ");
+            System.out.println();
+        }
     }
 }
