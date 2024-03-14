db72c18caef917330f9c854e4c067a9a77dcff1a
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-14 08:06:46 -0400

    The project was kinda a pain but it was worth it to understand that we can take the value of a string and convert it into an enum it also make me better a reading file and understanding regex

--- src/main/java/Question2/Library.java
+++ src/main/java/Question2/Library.java
@@ -1,48 +1,113 @@
 package Question2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Library {
-    String bookName;
-    int numberOfCopy;
-    String authorName;
-    public  void descriptionType(){
-        descriptionType();
+
+    String id;
+    static int nextId = 0;
+    String title ;
+
+    public void printDetails(){
+        System.out.println("Title: " + getTitle() + "Id " + getId() );
     }
 
-    public Library(String bookName, int numberOfCopy, String authorName) {
-        this.bookName = bookName;
-        this.numberOfCopy = numberOfCopy;
-        this.authorName = authorName;
-    }
-}
-class HorrorTypeOfBook extends Library{
-
-    public HorrorTypeOfBook(String bookName, int numberOfCopy, String authorName) {
-        super(bookName, numberOfCopy, authorName);
+    public Library( String title) {
+        this.id = String.format("LOBY " + nextId++);
+        this.title = title;
     }
 
-    @Override
-    public void descriptionType() {
-        System.out.println("horror type ");
-    }
-}
-
-class AdventureTypeOfBook extends Library{
-    public AdventureTypeOfBook(String bookName, int numberOfCopy, String authorName) {
-        super(bookName, numberOfCopy, authorName);
+    public String getId() {
+        return id;
     }
 
-    @Override
-    public void descriptionType() {
-        System.out.println("Adventure type");
+    public int getNextId() {
+        return nextId;
     }
-}
-class Demo{
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNextId(int nextId) {
+        this.nextId = nextId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public static Library type(String fileName) throws IOException {
+        System.out.println("Make sure your file is in this specific format: " +
+                "Title(note that title must be first letter in upper case), TypeOf(Book / DVD), Number of Pages/ Duration, Genre(DVD.Genre.FICTION), authorName " );
+        System.out.println("Note that those in parentheses are just examples not an actual format only one space after the comma");
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        String line;
+        String title = "";
+        String bookType = "";
+        String name = "";
+        Genre genre = Genre.FICTION;
+        int durationOrNumPages = 0;
+        while((line = bufferedReader.readLine()) != null){
+            String[] parts = line.split(",");
+            for(String part : parts){
+                if(part.matches("[A-Z]{1}[a-z]{0,100}")){
+                  title = part.trim();
+                }
+                else if(part.trim().matches("Book")|| part.trim().matches("DVD")){
+                     bookType = part.trim();
+                }
+                else if(part.trim().matches("[0-9]+")){
+                     durationOrNumPages = Integer.parseInt(part.trim());
+                }
+                else if(part.trim().matches("(Genre\\.)([A-Z]+)")){
+                    String[] enumGets = part.split(".");
+                    for(String enumG : enumGets) {
+                        if(enumG.matches("[AZ]+"))
+                        genre = Genre.valueOf(enumG.trim().toUpperCase());
+                    }
+
+                } else if (part.matches(" [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+ ?([A-Z]{1}[a-z]+)?")) {
+                    name = part.trim();
+                }else {
+                    System.out.println("Wrong format ! " + part.trim());
+                }
+
+            }
+        }
+      return  getInstance(title,name,bookType,durationOrNumPages,genre);
+    }
+
     public static void main(String[] args) {
-        // static binding happens at compile time so the compiler know
-        Library book = new HorrorTypeOfBook("B",3,"j");
+        Library[] libraryObjects = {new Book("The Deep","Lopez", Genre.FICTION,100),
+        new DVD("The Ocean","Carl",30, Genre.ACTION)};
 
-        book.descriptionType(); // static binding this happens at compile time the compiler knows that
-        // the type of book is horror it happens at compile time
+        for(Library library : libraryObjects){
+            library.printDetails(); // late binding since it happens at run time only at runtime
+            // the compiler will know which print details needs to be called
+        }
+        try {
 
+
+            Library unknownObjectFromFile = type("/Users/josue/Library/Mobile Documents/com~apple~TextEdit/Documents/jo.txt");
+            unknownObjectFromFile.printDetails();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static Library getInstance(String title, String name,String type ,int durationOrPages, Genre genre){
+        if(type.equals("Book")){
+            return new Book(title, name, genre, durationOrPages);
+        } else  {
+            return new DVD(title,name,durationOrPages,genre);
+        }
     }
 }
